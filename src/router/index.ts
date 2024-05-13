2eca9ae9743db548720ed4ae3272debd477557f0
Liu-code3 <100696868+Liu-code3@users.noreply.github.com>
2024-05-13 16:19:59 +0800

    fix(eslint): @antfu/eslint-config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,24 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: "/",
-        redirect: "/login"
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/content.vue'),
+    meta: {
+      keepAlive: true, // 设置需要缓存的组件
     },
-    {
-        path: "/login",
-        name: "login",
-        component: () => import("../views/content.vue"),
-        meta: {
-            keepAlive: true, // 设置需要缓存的组件
-          },
-    }
+  },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 })
- 
+
 export default router
