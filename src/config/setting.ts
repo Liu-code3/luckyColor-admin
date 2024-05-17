ca490f41a05689212a8a38f749eadb06162f07f9
Liu-code3 <100696868+Liu-code3@users.noreply.github.com>
2024-05-17 11:17:11 +0800

    fix(eslint): eslint.config, setting

---
+++ src/config/setting.ts
@@ -0,0 +1,54 @@
+const colorList = [
+  {
+    key: '薄暮',
+    color: '#F5222D'
+  },
+  {
+    key: '火山',
+    color: '#FA541C'
+  },
+  {
+    key: '胭脂粉',
+    color: '#EB2F96'
+  },
+  {
+    key: '日暮',
+    color: '#FAAD14'
+  },
+  {
+    key: '明青',
+    color: '#13C2C2'
+  },
+  {
+    key: '极光绿',
+    color: '#52C41A'
+  },
+  {
+    key: '深绿',
+    color: '#009688'
+  },
+  {
+    key: '拂晓蓝（默认）',
+    color: '#4890F7'
+  },
+  {
+    key: '极客蓝',
+    color: '#2F54EB'
+  },
+  {
+    key: '酱紫',
+    color: '#722ED1'
+  },
+  {
+    key: '主题黑',
+    color: '#001529'
+  }
+];
+
+type FnType = <T>(colorWeak: T) => void;
+const updateColorWeak: FnType = (colorWeak) => {
+  const app = document.body.querySelector('#app');
+  colorWeak ? app?.classList.add('colorWeak') : app?.classList.remove('colorWeak');
+};
+
+export { colorList, updateColorWeak };
