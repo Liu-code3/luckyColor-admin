077926f7d2585e5eeb92abf319b94cde39d7ef64
Liu-code3 <100696868+Liu-code3@users.noreply.github.com>
2024-05-17 11:29:48 +0800

    fix(eslint) (#20)
    * docs(README)
    
    * feat(init): ts支持配置
    
    * feat(eslint): 配置eslint
    
    * feat(unocss): 配置unocss, 修改vite.config
    
    * feat(tsconfig): ts配置文件
    
    * fix(config): eslint配置
    
    * fix(eslint): @antfu/eslint-config
    
    * feat(utils): toolkit
    
    * fix(package.json): 解决合并冲突问题
    
    * feat(mock): mock-function
    
    * feat(errorHandler): add errorHandler
    
    * feat(icon): icon Register
    
    * fix(eslint): eslint.config, setting
    
    * chore(.gitignore): ignore auto-imports.d.ts, components.d.ts

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,32 @@
 import antfu from '@antfu/eslint-config';
 
-export default antfu({
-  ignores: [
-    '.vscode',
-    '**/.vscode/**',
-    'build/*.js',
-    'build/*.js/**',
-    'src/assets',
-    'src/assets/**',
-    'public',
-    '**/public/**',
-    'dist',
-    '**/dist/**',
-    'node_modules',
-    '**/node_modules/**',
-    'pnpm-lock.yaml',
-    '**/pnpm-lock.yaml/**',
-    '**/*.d.ts'
-  ],
-  formatters: true,
-  unocss: true,
-  vue: true,
-  typescript: true,
-  stylistic: {
-    indent: 2, // 4, or 'tab'
-    quotes: 'single', // or 'double'
-    semi: true // or false
-  },
-  rules: {
-    'no-unused-vars': 'error',
-    'no-undef': 'off',
-    'no-console': 'warn',
-    'antfu/if-newline': 'off',
-    'style/comma-dangle': ['error', 'never']
+export default antfu(
+  {
+    ignores: [
+      '.vscode',
+      'build/',
+      'src/assets',
+      'public',
+      'dist/',
+      'node_modules/',
+      'pnpm-lock.yaml',
+      '*.d.ts'
+    ],
+    formatters: true,
+    unocss: true,
+    vue: true,
+    typescript: true,
+    stylistic: {
+      indent: 2, // 4, or 'tab'
+      quotes: 'single', // or 'double'
+      semi: true // or false
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'off',
+      'no-console': 'warn',
+      'antfu/if-newline': 'off',
+      'style/comma-dangle': ['error', 'never']
+    }
   }
-});
+);
