5365270b34c6482083d5576a9d4978d7a3cc353f
sunYingQi <2260174317@qq.com>
2024-05-21 17:02:01 +0800

    feat(router):动态路由

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
-import { useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import tool from '@/utils/tool';
+import { useIconRender } from '@/hooks/iconRender';
 
+const iconRender = useIconRender();
 interface MenuItem {
   pid: number;
   id: number;
@@ -27,7 +29,7 @@ function transformMenuData(data: MenuItem[]): TransformedMenuItem[] {
     const newItem: TransformedMenuItem = {
       label: item.title,
       key: item.path,
-      icon: tool.iconRender(item.icon)
+      icon: iconRender(item.icon)
     };
     if (item.children && item.children.length > 0)
       newItem.children = transformMenuData(item.children);
@@ -40,11 +42,13 @@ const menuOptions: TransformedMenuItem[] = transformMenuData(menuData);
 
 const inverted = ref(false);
 const router = useRouter();
+const route = useRoute();
 
 function handleUpdateValue(key: string, item: string) {
   console.log(item);
   console.log(key);
   router.push(key);
+  console.log(route);
 }
 </script>
 
