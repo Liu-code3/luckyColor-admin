161c4f4198b914b594666d94e1a8ca37991136ff
webLXM <1944754630@qq.com>
2024-06-21 23:00:02 +0800

    feat:🌟富文本组件封装

---
+++ src/views/icomponent/editor.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Editor from '@/components/wangEditor/index.vue';
+
+const value = ref('初始内容');
+</script>
+
+<template>
+  <div class="p-15px">
+    <Editor v-model="value" style="height: calc(100vh - 180px)" />
+  </div>
+</template>
