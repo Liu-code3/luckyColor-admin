c68f284c67f032126ef0b78bf6f43da486ff7a24
webLXM <1944754630@qq.com>
2024-07-12 22:43:18 +0800

    fix(message): 🔨 naiveui message等反馈api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,4 @@
 import { createApp } from 'vue';
-import {
-  // component
-  NButton,
-  // create naive ui
-  create
-} from 'naive-ui';
 import '@/config/vxeTable';
 import store from './store';
 import App from './App.vue';
@@ -14,13 +8,8 @@ import 'virtual:uno.css';
 import './style.css';
 import 'animate.css';
 
-const naive = create({
-  components: [ NButton ]
-});
-
 const app = createApp(App);
 app.use(router);
 app.use(store);
-app.use(naive);
 app.use(luckyColor);
 app.mount('#app');
